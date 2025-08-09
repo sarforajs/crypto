a89ab6b0ec00a57b64fc58cd5f954e0fe90fd8bf
sarforajs <128588277+sarforajs@users.noreply.github.com>
2025-08-09 20:23:31 +0530

    Add files via upload

---
+++ src/main/resources/static/script.js
@@ -0,0 +1,26 @@
+function typeEffect(element, text, speed, callback) {
+    let i = 0;
+    function typing() {
+        if (i < text.length) {
+            element.innerHTML = text.substring(0, i + 1);
+            element.classList.add("typing");
+            i++;
+            setTimeout(typing, speed);
+        } else {
+            element.classList.remove("typing");
+            if (callback) callback();
+        }
+    }
+    typing();
+}
+
+// Start typing when page loads
+window.onload = function() {
+    const title = document.getElementById("title");
+    const footer = document.getElementById("footer");
+
+    typeEffect(title, " Crypto Encrypt / Decrypt", 100, function() {
+        // After title finishes, type footer
+        typeEffect(footer, "Project By: Sarforaj Shaikh & Faisal Mirza", 80);
+    });
+};
